12b25114edc6448a1f6f67e6955b90ec6e379d9c
r3nt0n <r3nt0n@protonmail.com>
2022-12-26 18:52:16 +0100

    keepass4brute 1.0

---
+++ README.md
@@ -0,0 +1,24 @@
+## About the project
+
+KDBX 4.x format (Keepass >=2.36) is not supported by `keepass2john` yet, so there is no known way to extract the hash and crack it.
+
+This tool is a quick and dirty patch to the current situation. It tries to bruteforce the passphrase testing a wordlist directly against db file.
+
+
+## Dependencies
+
++ keepassxc-cli
+
+
+## Usage
+
+```shell
+./keepass4brute.sh <kdbx-file> <wordlist>
+```
+
+
+## References
+
+JohnTheRipper related code comments and issue:
++ https://github.com/openwall/john/blob/ea33667eb5e1a650ecb6b4985125f4efa0955594/src/keepass2john.c#L81
++ https://github.com/openwall/john/issues/2502
---
+++ keepass4brute.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# https://github.com/r3nt0n/keepass4brute
+# Name: keepass4brute.sh
+# Author: r3nt0n
+# Version: 1.0 (25/11/2022)
+
+version="1.0"
+/bin/echo -e "\nkeepass4brute $version by r3nt0n"
+/bin/echo -e "https://github.com/r3nt0n/keepass4brute\n"
+
+
+if [ $# -ne 2 ]
+then
+  /bin/echo "Usage $0 <kdbx-file> <wordlist>"
+  exit 2
+fi
+
+dep="keepassxc-cli"
+command -v $dep >/dev/null 2>&1 || { /bin/echo >&2 "Error: $dep not installed.  Aborting."; exit 1; }
+
+n_total=$( wc -l < $2 )
+n_tested=0
+
+for line in $( /bin/cat $2 )
+do
+  n_tested=$((n_tested + 1))
+  /bin/echo -ne "[+] Words tested: $n_tested/$n_total ($line)                                          \r"
+
+  if ! /bin/echo $line | keepassxc-cli open $1 2>&1 | /bin/grep -q "Error"
+  then
+    /bin/echo -ne "\n"
+    /bin/echo "[*] Password found: $line"; exit 0;
+  fi
+done
+
+/bin/echo -ne "\n"
+/bin/echo "[!] Wordlist exhausted, any match found"; exit 3;
